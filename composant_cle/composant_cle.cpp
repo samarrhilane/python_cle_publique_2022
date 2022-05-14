6b0d9682330a230e8d0e7002214b7c47853dfa9a
samarrhilane <samarrhilane@gmail.com>
2022-05-14 13:35:19 +0200

    Update composant_cle.cpp

--- composant_cle/composant_cle.cpp
+++ composant_cle/composant_cle.cpp
@@ -1,6 +1,48 @@
 #include <pybind11/pybind11.h>
+#include <stddef.h>
 
-char version[]="1.0";
+
+
+unsigned char hexchr2bin(const char hex)
+{
+	unsigned char result;
+
+	if (hex >= '0' && hex <= '9') {
+		result = hex - '0';
+	} else if (hex >= 'A' && hex <= 'F') {
+		result = hex - 'A' + 10;
+	} else if (hex >= 'a' && hex <= 'f') {
+		result = hex - 'a' + 10;
+	} else {
+		return 0;
+	}
+	return result;
+}
+
+
+
+void hexStringToBin(unsigned char *out,const char * hexPrivate) {
+    for (int i=0; i<32; i++){
+	out[i] = hexchr2bin(hexPrivate[2*i])<<4 | hexchr2bin(hexPrivate[2*i+1]);
+    }
+}
+
+
+char *binToHexString(char *out,const unsigned char *bin, size_t len)
+{
+    size_t  i;
+
+    if (bin == NULL || len == 0)
+	return NULL;
+
+    for (i=0; i<len; i++) {
+	out[i*2]   = "0123456789abcdef"[bin[i] >> 4];
+	out[i*2+1] = "0123456789abcdef"[bin[i] & 0x0F];
+    }
+    out[len*2] = '\0';
+
+    return out;
+}
 
 class Cle {
   
