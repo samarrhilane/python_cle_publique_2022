6cb4e188e9f72ac012beeaa5b43c2f187b03de1f
samarrhilane <samarrhilane@gmail.com>
2022-05-15 16:05:41 +0200

    Update composant_cle.cpp

--- composant_cle/composant_cle.cpp
+++ composant_cle/composant_cle.cpp
@@ -1,114 +1,12 @@
 #include <pybind11/pybind11.h>
+#include <string>
+#include <stdexcept>
 #include "micro-ecc/uECC.h"
-char version[]="1.0";
-
-char const* getVersion() {
-	return version;
-}
-
-uint8_t hexchr2bin(const char hex)
-{
-	uint8_t result;
-
-	if (hex >= '0' && hex <= '9') {
-		result = hex - '0';
-	} else if (hex >= 'A' && hex <= 'F') {
-		result = hex - 'A' + 10;
-	} else if (hex >= 'a' && hex <= 'f') {
-		result = hex - 'a' + 10;
-	} else {
-		return 0;
-	}
-	return result;
-}
-
-
-
-void hexStringToBin(uint8_t *out,const char * hexPrivate) {
-    for (int i=0; i<32; i++){
-	out[i] = hexchr2bin(hexPrivate[2*i])<<4 | hexchr2bin(hexPrivate[2*i+1]);
-    }
-}
-
-
-char *binToHexString(char *out,const unsigned char *bin, size_t len)
-{
-    size_t  i;
-
-    if (bin == NULL || len == 0)
-	return NULL;
-
-    for (i=0; i<len; i++) {
-
-	out[i*2]   = "0123456789abcdef"[bin[i] >> 4];
-	out[i*2+1] = "0123456789abcdef"[bin[i] & 0x0F];
-    }
-    out[len*2] = '\0';
-
-    return out;
-}
+#include "micro-ecc/uECC_vli.h"
 
 
 
 
-
-
-class Cle
-{
-    public:
-        Cle(){}
-        ~Cle() {}
-
-        void initialize(std::string &nb) { 
-		PrivateKey=nb;
-		uint8_t binaryPrivate[32];
-		hexStringToBin(binaryPrivate,PrivateKey.c_str());
-		const int publicKeySize=uECC_curve_public_key_size(uECC_secp256k1());
-		uint8_t *varIntPublicKey = new uint8_t[publicKeySize];
-		uECC_compute_public_key(binaryPrivate,varIntPublicKey,uECC_secp256k1());
-		char hexPublicKey[128];
-		binToHexString(hexPublicKey,varIntPublicKey,64);
-		PublicKey=std::string(hexPublicKey,128);
-		//PublicKey=std::string( varIntPublicKey, varIntPublicKey+publicKeySize );
-		}
-		
-        const std::string &getPrivateKey() const { return PrivateKey; }
-		const std::string &getPublicKey() const { return PublicKey; }
-
-    private:
-        std::string PublicKey;
-        std::string PrivateKey;
-};
- 
-namespace py = pybind11;
-
-
-PYBIND11_MODULE(cle_component,greetings)
-{
-  greetings.doc() = "greeting_object 1.0";
-  greetings.def("getVersion", &getVersion, "a function returning the version");
-  
-   // bindings to Cle class
-    py::class_<Cle>(greetings, "Cle", py::dynamic_attr())
-        .def(py::init())
-        .def("initialize", &Cle::initialize)
-        .def("getPrivateKey", &Cle::getPrivateKey)
-        .def("getPublicKey", &Cle::getPublicKey);
-}
-
-
-
-
-
-// #include <pybind11/pybind11.h>
-// #include <string>
-// #include <stdexcept>
-// #include "micro-ecc/uECC.h"
-// #include "micro-ecc/uECC_vli.h"
-
-
-
-/*
 
 // Convert charactere to binary
 unsigned char hexchr2bin(const char hex)
@@ -179,7 +77,7 @@ Returns the size of a public key for the curve in bytes.
 //Returns 1 if the key pair was generated successfully, 0 if an error occurred.
 //int uECC_make_key(uint8_t *public_key, uint8_t *private_key, uECC_Curve curve);*/
 
-/*
+
 class Cle {
   
     
@@ -190,13 +88,14 @@ class Cle {
 	
 	void initialize(std::string &Number) { 
 // 		const struct uECC_Curve_t *curve = uECC_secp256k1(); // create ECC type secp256k1 : elliptic curve used by Bitcoin
-// 		uint8_t Private_Key[32];
 // 		int Private_Key_Size = uECC_curve_private_key_size(curve);
+// 		int Public_Key_Size = uECC_curve_public_key_size(curve);
+// 		uint8_t B_Private_Key[32];
 // 	        Unit_Private_Key = uint8_t l_secret1[Private_Key_Size];
 // 		std::string &output;
 // 		PrivateKey = hexStringToBin(output, Number);
 // 		std::string &output2;
-// 		int Public_Key_Size = uECC_curve_public_key_size(curve);
+		
 // 		PublicKey = binToHexString(output2, output, ?);
 		
 		
@@ -212,9 +111,7 @@ class Cle {
 		
 		}
 		
-   private:
-        std::string PublicKey;
-        std::string PrivateKey;	
+  
         //
         }; 
         
@@ -223,17 +120,20 @@ class Cle {
 	std::string getPublicKey()  {
          return PublicKey; }
         
+   private:
+        std::string PublicKey;
+        std::string PrivateKey;	
  }
         
         
  namespace py = pybind11;
  
- PYBIND11_MODULE(cle_component,KEY) {
-   py::class_<Cle>(KEY, "Cle",py::dynamic_attr())
+ PYBIND11_MODULE(composant_cle,greetings) {
+   py::class_<Cle>(greetings, "Cle",py::dynamic_attr())
       	.def(py::init<>())
-	.def("initialize", &Key::initialize) 
-        .def("getPrivateKey", &Key::getPrivateKey)
-        .def("getPublicKey", &Key::getPublicKey);
+	.def("initialize", &greetings::initialize) 
+        .def("getPrivateKey", &greetings::getPrivateKey)
+        .def("getPublicKey", &greetings::getPublicKey);
  }
      
-*/
+
